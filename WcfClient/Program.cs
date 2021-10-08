6170253b7f85182405a494f7d2271abe1b83627e
jasondel <jasondel@WINDOWS2016-SAM>
2021-10-08 19:31:12 +0000

    Changed to wsHttpBinding

--- WcfClient/Program.cs
+++ WcfClient/Program.cs
@@ -11,12 +11,14 @@ namespace WcfClient
             {
                 string address = "http://localhost:" + args[0] + "/LibraryService";
                 client = new LibraryServiceSolution.LibraryServiceClient(
-                    "BasicHttpBinding_ILibraryService",
+                    "LibraryService_EndPoint",
                     address);
             }
             else
             {
-                client = new LibraryServiceSolution.LibraryServiceClient();
+                Console.WriteLine("Error, must specify a port number.");
+                Console.WriteLine("\texample: WcfClient 8888");
+                return;
             }
 
             LibraryServiceSolution.Book book = client.SearchBook("Name 12");
